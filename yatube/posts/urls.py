3c2bd71fa877a72c23171a975b2fd00e70e0e208
Дмитрий <pvpwarsss@gmai.com>
2022-09-18 19:26:53 +0300

    UNREALEASE VERSION

---
+++ yatube/posts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index),
+    # Список мороженого
+    path('posts/', views.group_list),
+    # Подробная информация о мороженом. Ждем пременную типа int,
+    # и будем использовать ее под именем pk
+    path(
+        'group'
+        '/<slug:slug>/',
+        views.group_posts
+    ),
+]
